802888687c5533352d11747c6dcf7db923520d6e
rumagishi <rumagishi@gmail.com>
2015-01-06 11:26:03 +0900

    enable to output per 10 second

--- exp2_myzt/toten.py
+++ exp2_myzt/toten.py
@@ -1,19 +1,22 @@
-#import pandas as pd
-#
-#df = pd.read_csv('sample_9_csv.log', index_col='time')
-#print df
-
 import csv
+from fetchtime import fetchtime
+
+low=0
+high=10
+_ave=0
+ave=0
+counter=0
+sec=0
 
 with open('sample_9_csv.log', 'r') as f:
     reader = csv.reader(f)
     header = next(reader)
 
     for row in reader:
-        if row[3]=='0':
+        if row[3] == '0':
             value = int(row[1])
             if value<0: 
-                value = value + 256
+                value += 256
             else:
                 pass
             value = value / 2 - 121
@@ -21,11 +24,30 @@ with open('sample_9_csv.log', 'r') as f:
 
             value = int(row[2])
             if value<0: 
-                value = value + 256
+                value += 256
             else:
                 pass
             value = value / 2 - 121
             row[2] = str(value)
+
+            sec = fetchtime(row[0])
+            if low<=sec<high:
+                _ave += float(row[1])
+                counter += 1
+            elif high<=sec:
+                ave = _ave / counter
+                _ave = float(row[1])
+                counter = 1
+                low += 10
+                high += 10
+            #if sec%10 == 0 and sec != 0:
+                print(str(low) + "," + str(ave))
         else:
             pass
-        print(','.join(row))
+        #print(','.join(row))
+        #if time%10 == 0:
+        #    _ave = 0 
+        #    low += 10
+        #    high += 10
+        #else:
+        #    pass
